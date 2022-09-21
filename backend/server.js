aa6ae9939caa7843c63670f991ae5498836c21ed
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-09-21 19:00:43 +0200

    connect db to mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const sneakerRoutes = require("./routes/sneakers");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -14,7 +15,15 @@ app.use((req, res, next) => {
 // routes
 app.use("/sneakers", sneakerRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000");
-});
+// connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port 4000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
