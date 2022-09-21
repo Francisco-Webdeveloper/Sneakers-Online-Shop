3aa65058b39ee52c1165c144b25375a7d441b4e8
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-09-22 00:09:08 +0200

    add controller and define the POST handler function

---
+++ backend/controllers/sneakersController.js
@@ -0,0 +1,51 @@
+const Sneakers = require("../models/sneakersModel");
+
+// get all sneakers
+
+// get a single pair of sneakers
+
+// create a new pair of sneakers
+const createSneakers = async (req, res) => {
+  const { brand, name, description, price, discountPrice, stock } = req.body;
+  const {
+    image1,
+    imageThumbnail1,
+    image2,
+    imageThumbnail2,
+    image3,
+    imageThumbnail3,
+    image4,
+    imageThumbnail4,
+  } = req.body.url;
+
+  // add doc to db
+  try {
+    const sneakers = await Sneakers.create({
+      brand,
+      name,
+      description,
+      price,
+      discountPrice,
+      stock,
+      url: {
+        image1,
+        imageThumbnail1,
+        image2,
+        imageThumbnail2,
+        image3,
+        imageThumbnail3,
+        image4,
+        imageThumbnail4,
+      },
+    });
+    res.status(200).json(sneakers);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a pair of sneakers
+
+// update a pair of sneakers
+
+module.exports = { createSneakers };
--- backend/routes/sneakers.js
+++ backend/routes/sneakers.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Sneakers = require("../models/sneakersModel");
+const { createSneakers } = require("../controllers/sneakersController");
 
 // instance of the router
 const router = express.Router();
@@ -12,43 +12,7 @@ router.get("/:id", (req, res) => {
   res.json({ msg: "GET a single pair of sneaker!" });
 });
 
-router.post("/", async (req, res) => {
-  const { brand, name, description, price, discountPrice, stock } = req.body;
-  const {
-    image1,
-    imageThumbnail1,
-    image2,
-    imageThumbnail2,
-    image3,
-    imageThumbnail3,
-    image4,
-    imageThumbnail4,
-  } = req.body.url;
-
-  try {
-    const sneakers = await Sneakers.create({
-      brand,
-      name,
-      description,
-      price,
-      discountPrice,
-      stock,
-      url: {
-        image1,
-        imageThumbnail1,
-        image2,
-        imageThumbnail2,
-        image3,
-        imageThumbnail3,
-        image4,
-        imageThumbnail4,
-      },
-    });
-    res.status(200).json(sneakers);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createSneakers);
 
 router.delete("/:id", (req, res) => {
   res.json({ msg: "DELETE a pair of sneaker!" });
