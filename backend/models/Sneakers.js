e72d2b66f57bfc20cb3de6ad484a306cab864b07
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-09-21 21:31:18 +0200

    add schema and model

---
+++ backend/models/Sneakers.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const sneakersSchema = new Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: false,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    url: {
+      image1: {
+        type: String,
+        required: true,
+      },
+      imageThumbnail1: {
+        type: String,
+        required: true,
+      },
+      image2: {
+        type: String,
+        required: true,
+      },
+      imageThumbnail2: {
+        type: String,
+        required: true,
+      },
+      image3: {
+        type: String,
+        required: true,
+      },
+      imageThumbnail3: {
+        type: String,
+        required: true,
+      },
+      image4: {
+        type: String,
+        required: true,
+      },
+      imageThumbnail4: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Sneakers", sneakersSchema);
