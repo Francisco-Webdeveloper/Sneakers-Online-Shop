04261760c52d9edac854ba0b8c7de11381423338
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-09-21 15:44:06 +0200

    implement middleware to log out requests coming in

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,16 @@ const express = require("express");
 
 const app = express();
 
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req, method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ msg: "welcome to the app" });
+});
+
 // listen for requests
 app.listen(process.env.PORT, () => {
   console.log("listening on port 4000");
