4373235f9d1da4a545682844416761aa1c338f1d
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-09-28 13:08:54 +0200

    create Product component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header";
+import Product from "./components/Product";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Product />
     </div>
   );
 };
