ac626deecf0cfed731caf2a42422455b0c5dba81
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-09-28 13:08:20 +0200

    install and implement  cors package

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const sneakerRoutes = require("./routes/sneakers");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
