7321514b4f9b6be461c079bbca9f305783252879
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-10-01 00:57:09 +0200

    wrap all the pages / components with ContextProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.scss";
 import Header from "./components/Header";
-import Product from "./components/Product";
+import ProductPage from "./pages/ProductPage";
+import { ContextProvider } from "./context/sneakersContext";
 
 const App = () => {
   return (
     <div className="container">
-      <Header />
-      <Product />
+      <ContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sneaker/:sneakerId" element={<ProductPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ContextProvider>
     </div>
   );
 };
